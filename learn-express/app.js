dc13770d73a207674c7f65c1f468a1b6e7ad6ce1
juicyjusung <jusung21c@gmail.com>
2019-01-05 14:49:52 +0900

    connect-flash 설치 및 테스트
    일회성 메세지 생성

--- learn-express/app.js
+++ learn-express/app.js
@@ -4,6 +4,7 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var session = require('express-session');
+var flash = require('connect-flash');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -27,6 +28,7 @@ app.use(session({
     secure: false,
   }
 }));
+app.use(flash());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
